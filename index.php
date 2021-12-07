ada11ea102d121d79d23ad72817a283abe28e929
QuentinChevaux <quentin.chevaux@gmail.com>
2021-12-07 13:52:22 +0100

    PHP

--- index.php
+++ index.php
@@ -16,22 +16,83 @@
 
     <?php
 
-    $titre = "Titre de la Page PHP";
+        $titre = "Titre de la Page PHP";
 
-    echo "<h1> $titre </h1>";
+        echo "<h1> $titre </h1>";
 
-    ?>
+        $a = 2;
+        $b = 5;
 
-    <?php
+        $calcul = $a + $b;
 
-    $a = 2;
-    $b = 5;
-
-    $calcul = $a + $b;
-
-    echo "<div class = 'calcul'><p> Résultat du Calcul : $calcul </p></div>";
+        echo "<div class = 'calcul'><p> Résultat duCalcul : $calcul </p></div>";
 
     ?>
+
+    <form action="traitement.php" method="POST">
+
+        <input type="text" placeholder="Tapez votre Nom" name='nom' autofocus />
+        
+        <input type="text" placeholder="Tapez votre Pseudo" name="pseudo" />
+
+            <br />
+            <br />
+
+        <input type="password" name="mdp" placeholder="Tapez votre Mot de Passe"/>
+        <input type="password" name="mdp_confirm" placeholder="Confirmez votre Mot de Passe" />
+
+        <p> Vous êtes : </p>
+
+        <label for="homme">Un Homme</label>
+        <input type="radio" name="sexe" value="homme" id="homme" checked />
+
+            <br />
+
+        <label for="femme">Une Femme</label>
+        <input type="radio" name="sexe" value="femme" id="femme" />
+
+            <br />
+            <br />
+
+        <input type="range" name="range" list="range_list" min="1" max="10" step="1"/>
+
+        <datalist id="range_list">
+
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+
+        </datalist>
+
+            <br />
+            <br />
+
+        <label for="action">Action</label>
+        <input type="checkbox" id="action" name="action">
+
+            <br />
+
+        <label for="comedie">Comédie</label>
+        <input type="checkbox" id="comedie" name="comedie">
+
+            <br />
+
+        <label for="sf">Science Fiction</label>
+        <input type="checkbox" id="sf" name="sf" />
+
+            <br />
+            <br />
+        
+        <input type="submit" value="Valider" />
+
+    </form>
 
 </body>
 
---
+++ traitement.php
@@ -0,0 +1,104 @@
+<?php
+
+    if (isset($_POST["nom"])){
+
+        echo "Votre nom est : " . $_POST["nom"] . "<br />";
+
+    }
+
+    if (isset($_POST["pseudo"]) && !empty($_POST["pseudo"])) {
+
+        echo "Votre Pseudo est : " . $_POST["pseudo"]. "<br />";
+
+    }
+
+    else {
+
+        echo "Saisissez un Pseudo <br /> ";
+
+    }
+
+    if (isset($_POST["mdp"]) && !empty($_POST["mdp"]) && isset($_POST["mdp_confirm"]) && !empty($_POST["mdp_confirm"])) {
+
+        if ($_POST["mdp"] == $_POST["mdp_confirm"]) {
+
+            echo "Les mots de passes sont identiques <br />";
+
+        }
+
+        else {
+
+            echo "Les Mots de Passe sont Différents ! <br /> ";
+
+        }
+
+    }
+
+    else {
+
+        echo "Saisir tous les Mots de Passe ! <br />";
+
+    }
+
+    if (isset($_POST["sexe"])) {
+
+        if ($_POST["sexe"] == "homme") {
+
+            echo "Sexe : Homme <br />";
+
+        }
+
+        else {
+
+            echo "Sexe : Femme <br />";
+
+        }
+
+    }
+
+    if (isset($_POST["range"]) && !empty($_POST["range"])) {
+
+        echo "Numéro = " . $_POST["range"] . "<br />";
+
+    }
+
+    echo "Les films Cochés : <ul>";
+
+
+        if ( isset($_POST["action"])) { 
+                
+            if ($_POST["action"] == "on" ) {
+
+                echo "<li>Action</li>";
+    
+            }
+
+        }
+
+        if  (isset($_POST["comedie"])) {
+
+            if ($_POST["comedie"] == "on" ) {
+
+                echo "<li>Comedie</li>";
+    
+            }
+
+        }
+            
+        if (isset($_POST["sf"])) {
+
+            if ($_POST["sf"] == "on" ) {
+
+                echo "<li>Science Fiction</li>";
+    
+            }
+
+        }
+
+    echo "</ul>";
+
+?>    
+
+<br />
+
+<a href="index.php">Retour</a>

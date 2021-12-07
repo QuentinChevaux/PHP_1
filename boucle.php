52068e218d0417a7dea2a3cbbcb6887da86fb1a7
QuentinChevaux <quentin.chevaux@gmail.com>
2021-12-07 16:32:28 +0100

    PHP

---
+++ boucle.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Document</title>
+
+</head>
+
+<body>
+
+    <?php /*
+
+        $i = 1;
+        $nombre = 12;
+
+        while ( $i < $nombre ) {
+
+            echo " $i moutons";
+            $i++;
+
+        }
+
+    */ ?>
+
+    <form action="" method="POST">
+
+        <label for="number">Entrez un Nombre : </label>
+        <input type="number" id="number" name="number" />
+
+        <input type="submit" value="Ajouter 10 à votre Chiffre">
+
+    </form>
+
+    <br />
+
+    <?php
+
+    if (isset($_POST["number"])) {
+
+        $j = $_POST["number"] + 1;
+    
+        while ( $j <= ( $_POST["number"] + 10) ) {
+    
+            echo "$j <br />";
+    
+            $j++;
+    
+        }
+
+    }
+
+?>
+    
+</body>
+</html>
--- formulaire.php
+++ formulaire.php
@@ -1,3 +1,35 @@
+<?php
+
+    if (isset($_POST["number"])) {
+
+        if ($_POST["number"] < 10 ) {
+
+            $message = "Plus grand";
+
+        }
+
+        else if ($_POST["number"] > 20) {
+
+            $message = "Plus petit";
+
+        }
+
+        else {
+
+            $message = "Nombre entre 10 et 20";
+
+        }
+
+    }
+
+    else {
+
+        $message = "";
+
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,8 +43,8 @@
 
     <form action="" method="POST">
 
-        <label for="nb">Saisir un Nombre :</label>
-        <input type="number" id="nb" name="nb" />
+        <label for="number">Saisir un Nombre :</label>
+        <input type="number" id="number" name="number" />
 
         <input type="submit" value="Valider" />
 

262971b4849e51523ce577e1c11d35cf68c9776c
QuentinChevaux <quentin.chevaux@gmail.com>
2021-12-09 16:44:34 +0100

    PHP

--- boucle.php
+++ boucle.php
@@ -136,15 +136,147 @@
 
             }
 
-            echo "</div>";
+                echo "</div>";
 
             echo "</div>";
 
+        }
+
+        echo "</div>";
+
+        ?>
+
+            <br />
+            <br />
+
+        <form action="" method="POST">
+
+        <label for="largeur">Entrez une Largeur : </label>
+        <input type="number" id="largeur" name="largeur" />
+
+        <label for="radio_largeur">cm</label>
+        <input type="radio" name="radio_largeur" value="cm"/>
+        <label for="m_largeur">m</label>
+        <input type="radio" name="radio_largeur" value="m"/>
+
+        <br />
+        <br />
+
+        <label for="hauteur">Entrez une Hauteur : </label>
+        <input type="number" id="hauteur" name="hauteur" />
+
+        <label for="radio_hauteur">cm</label>
+        <input type="radio" name="radio_hauteur" value="cm" />
+        <label for="m_hauteur">m</label>
+        <input type="radio" name="radio_hauteur" value="m" />
+
+        <br />
+        <br />
+
+        <label for="perimetre">Périmètre</label>
+        <input type="radio" name="radio" value="perimetre"/>
+
+        <label for="surface">Surface</label>
+        <input type="radio" name="radio" value="surface"/>
+
+        <label for="radio_resultat">cm</label>
+        <input type="radio" name="radio_resultat" value="cm" />
+        <label for="radio_resultat">m</label>
+        <input type="radio" name="radio_resultat" value="m" />
+
+        <br />
+        <br />
+
+        <input type="submit" value="Calculer">
+
+
+        </form>
+
+            <br />
+            <br />
+
+        <?php
+        
+        if (isset($_POST["largeur"]) && !empty($_POST["largeur"]) && isset($_POST["hauteur"]) && !empty($_POST["hauteur"]) && isset($_POST["radio"])) {
+
+            if (isset($_POST["radio_largeur"]) && isset($_POST["radio_hauteur"]) && isset($_POST["radio_resultat"])) {
+
+                $largeur = 0;
+                $hauteur = 0;
+
+                if ( $_POST["radio_largeur"] == $_POST["radio_resultat"]) {
+
+                    $largeur = $_POST["largeur"];                    
+
+                }
+
+                else {
+                    
+                    if ( $_POST["radio_resultat"] == "cm") {
+
+                        
+                        $largeur = $_POST["largeur"] * 100;   
+
+                    }
+
+                    else {
+
+                        $largeur = $_POST["largeur"] / 100;
+
+                    }
+
+                }
+                
+                if ( $_POST["radio_hauteur"] == $_POST["radio_resultat"]) {
+
+                    $hauteur = $_POST["hauteur"];                    
+
+                }
+
+                else {
+                    
+                    if ( $_POST["radio_resultat"] == "cm") {
+
+                        $hauteur = $_POST["hauteur"] * 100;   
+
+                    }
+
+                    else {
+
+                        $hauteur = $_POST["hauteur"] / 100;
+
+                    }
+
+                }             
+        
+                if ( $_POST["radio"] == "perimetre") {
+        
+                    $perimetre = 2 * ($largeur + $hauteur);
+        
+                    echo "<h2>Le Périmètre avec largeur = $largeur et hauteur = $hauteur est : $perimetre</h2>";
+    
+                }
+    
+                if ( $_POST["radio"] == "surface") {
+        
+                    $surface = $largeur * $hauteur;
+        
+                    echo "<h2>L'Aire avec largeur = $largeur et hauteur = $hauteur est : $surface</h2>";
+    
+                }           
+
+            }
 
 
         }
 
+        else {
+
+            echo "Saisissez les Valeurs dans les Inputs";
+
+        }
+        
         ?>
-    
+
 </body>
 </html>

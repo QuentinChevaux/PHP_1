546bcdc32da28d946d400efd26ec137c4aa7bd11
QuentinChevaux <quentin.chevaux@gmail.com>
2021-12-09 10:03:39 +0100

    PHP

--- boucle.php
+++ boucle.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link rel="stylesheet" href="./index.css">
+
     <title>Document</title>
 
 </head>
@@ -36,23 +38,35 @@
 
     <br />
 
-    <?php
 
-    if (isset($_POST["number"])) {
 
-        $j = $_POST["number"] + 1;
-    
-        while ( $j <= ( $_POST["number"] + 10) ) {
-    
-            echo "$j <br />";
-    
-            $j++;
-    
+        <?php
+
+        if (isset($_POST["number"])) {
+
+            $j = $_POST["number"] + 1;
+
+            echo "<h2>Incrementation avec Boucle While : <br /></h2>";
+        
+            while ( $j <= ( $_POST["number"] + 10) ) {
+        
+                echo "<h2>$j <br /></h2>";
+        
+                $j++;
+        
+            }
+
+            echo "<br /><h2>Incrementation avec Boucle For : <br /></h2>";
+
+            for ( $j = $_POST["number"] + 1; $j <= $_POST["number"] + 10; $j++) {
+
+                echo "<h2>$j <br /></h2>";
+
+            }
+
         }
 
-    }
-
-?>
+        ?>
     
 </body>
 </html>
